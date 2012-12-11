0c20b2e7540ec92eb35a57a0e2b2c921cec2db38
David O'Trakoun <me@davidosomething.com>
2012-12-11 14:15:29 -0500

    quickpress is a side widget

--- remove-quickpress.php
+++ remove-quickpress.php
@@ -4,6 +4,6 @@
  */
 function dko_remove_quickpress() {
   global $wp_meta_boxes;
-  unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_quick_press']);
+  unset($wp_meta_boxes['dashboard']['normal']['side']['dashboard_quick_press']);
 }
 add_action('wp_dashboard_setup', 'dko_remove_quickpress');
